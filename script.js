7a12d47a33da058bdb4bbdd4b730e10d5f00bc91
Dmitriy Domrachev <dimd1288@gmail.com>
2022-09-12 15:43:08 +0300

    work completely done

---
+++ script.js
@@ -0,0 +1,46 @@
+let root = document.querySelector('.root');
+let profile = document.querySelector('.profile');
+let profileName = profile.querySelector('.profile__name');
+let profileAbout = profile.querySelector('.profile__about');
+let profileEditButton = document.querySelector('.profile__edit-button');
+let profileEditPopup = document.querySelector('.popup');
+let profileEditPopupContainer = profileEditPopup.querySelector('.popup__container');
+let profileEditPopupForm = profileEditPopupContainer.querySelector('.popup__form');
+let profileEditPopupSaveButton = profileEditPopupContainer.querySelector('.popup__save-button');
+let profileEditPopupCloseIcon = profileEditPopupContainer.querySelector('.popup__close-icon');
+let profileEditPopupName = profileEditPopupContainer.querySelector('.popup__input_type_name');
+let profileEditPopupAbout = profileEditPopupContainer.querySelector('.popup__input_type_about');
+
+
+function openEditProfilePopup(){
+    profileEditPopup.classList.add('popup_opened');
+    root.classList.add('root_overflow_hidden')
+    let name = profileName.textContent;
+    let about = profileAbout.textContent;
+    profileEditPopupName.value = name;
+    profileEditPopupAbout.value = about;
+}
+
+function closeEditProfilePopup(event){
+    const target = event.target;
+    if (!target.closest('.popup__container') || target.closest('.popup__close-icon') || target.closest('.popup__save-button')){
+        profileEditPopup.classList.remove('popup_opened');
+        root.classList.remove('root_overflow_hidden');
+    }
+}
+
+function formSubmitHandler (event) {
+    event.preventDefault(); 
+    let nameFromInput = profileEditPopupName.value;
+    let aboutFromInput = profileEditPopupAbout.value;
+    profileName.textContent = nameFromInput;
+    profileAbout.textContent = aboutFromInput;
+}
+
+profileEditButton.addEventListener('click', openEditProfilePopup);
+
+profileEditPopup.addEventListener('click', closeEditProfilePopup);
+
+profileEditPopupCloseIcon.addEventListener('click', closeEditProfilePopup);
+
+profileEditPopupForm.addEventListener('submit', formSubmitHandler); 
