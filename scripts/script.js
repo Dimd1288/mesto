cbc2c6c4d18b79b482abc409a9777840aa28086e
Dmitriy Domrachev <dimd1288@gmail.com>
2022-10-14 14:32:16 +0300

    Fields validation added

--- scripts/script.js
+++ scripts/script.js
@@ -9,16 +9,16 @@ const profileEditPopup = document.querySelector('#edit-profile');
 const profileEditPopupContainer = profileEditPopup.querySelector('.popup__container');
 const profileEditPopupForm = profileEditPopupContainer.querySelector('.popup__form');
 const profileEditPopupCloseIcon = profileEditPopupContainer.querySelector('.popup__close-icon');
-const profileEditPopupNameInput = profileEditPopupContainer.querySelector('.popup__input_type_name');
-const profileEditPopupAboutInput = profileEditPopupContainer.querySelector('.popup__input_type_about');
+const profileEditPopupNameInput = profileEditPopupContainer.querySelector('#name-input');
+const profileEditPopupAboutInput = profileEditPopupContainer.querySelector('#about-input');
 
 const addPlaceButton = document.querySelector('.profile__add-button');
 const addPlacePopup = document.querySelector('#add-place');
 const addPlacePopupContainer = addPlacePopup.querySelector('.popup__container');
 const addPlacePopupForm = addPlacePopupContainer.querySelector('.popup__form');
 const addPlacePopupCloseIcon = addPlacePopupContainer.querySelector('.popup__close-icon');
-const addPlacePopupTitleInput = addPlacePopupContainer.querySelector('.popup__input_type_title');
-const addPlacePopupLinkInput = addPlacePopupContainer.querySelector('.popup__input_type_link');
+const addPlacePopupTitleInput = addPlacePopupContainer.querySelector('#title-input');
+const addPlacePopupLinkInput = addPlacePopupContainer.querySelector('#link-input');
 const zoomPlacePopup = document.querySelector('#element-popup');
 const popupImage = zoomPlacePopup.querySelector('.popup__image');
 const popupCaption = zoomPlacePopup.querySelector('.popup__caption');
@@ -32,6 +32,11 @@ function openPopup(popup){
 
 function closePopup(popup){
   popup.classList.remove('popup_opened');
+  const formElement = popup.querySelector('.popup__form');
+  const inputsList = Array.from(formElement.querySelectorAll('.popup__input'));
+  inputsList.forEach((inputElement) => {
+    hideError(formElement, inputElement);
+  });
 }
 
 function openEditProfilePopup() {
@@ -91,7 +96,7 @@ function loadLikeButtonClickListener(element) {
 
 function loadBasketButtonClickListener(element) {
   const cardBasketIcon = element.querySelector('.element__basket');
-  cardBasketIcon.addEventListener('click', (event) => {
+  cardBasketIcon.addEventListener('click', () => {
     element.remove();
   })
 }
@@ -134,3 +139,48 @@ addPlacePopupForm.addEventListener('submit', addPlaceFormSubmitHandler);
 
 closeZoomPlacePopupIcon.addEventListener('click', closeZoomPlacePopup);
 
+// Отсюда начинаем валидацию
+
+function validateInput(formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showError(formElement, inputElement);
+  } else {
+    hideError(formElement, inputElement);
+  }
+}
+
+function showError(formElement, inputElement) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = inputElement.validationMessage;
+  errorElement.classList.add('popup__error_visible');
+};
+
+function hideError(formElement, inputElement) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__error_visible');
+  errorElement.textContent = '';
+}
+
+
+function addInputListeners(formElement) {
+  const inputsList = Array.from(formElement.querySelectorAll('.popup__input'));
+  inputsList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      validateInput(formElement, inputElement);
+    })
+  })
+}
+
+function enableValidation() {
+  const formsList = Array.from(document.querySelectorAll('.popup__form'));
+  formsList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    })
+    addInputListeners(formElement);
+  })
+}
+
+enableValidation();
