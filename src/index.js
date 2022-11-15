e27d37f62c4064a2dbb375ad4d1eceacc0829b88
Dmitriy Domrachev <dimd1288@gmail.com>
2022-11-15 10:22:05 +0300

    set webpack

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import Card from "./Card.js";
-import FormValidator from "./FormValidator.js";
-import PopupWithForm from "./PopupWithForm.js";
-import PopupWithImage from "./PopupWithImage.js";
-import Section from "./Section.js";
-import UserInfo from "./UserInfo.js";
+import Card from "./scripts/Card.js";
+import FormValidator from "./scripts/FormValidator.js";
+import PopupWithForm from "./scripts/PopupWithForm.js";
+import PopupWithImage from "./scripts/PopupWithImage.js";
+import Section from "./scripts/Section.js";
+import UserInfo from "./scripts/UserInfo.js";
+import './pages/index.css';
 
 const initialCards = [
   {
